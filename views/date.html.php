08ced928403ceb2ed6e78b01c657ee1405a62182
KevBurnsJr <kevburnsjr@gmail.com>
2010-06-09 03:46:55 -0700

    Fixing github link to open in new window.

--- views/date.html.php
+++ views/date.html.php
@@ -47,7 +47,7 @@
         <? if($network == "irc.freenode.net") { ?>
         <li class="webchat"><a href="http://webchat.freenode.net/?channels=<?=$channel?>" target="_blank">Freenode WebChat</a>
         <? } ?>
-        <li class="github"><a href="http://github.com/KevBurnsJr/irc-log-viewer">Fork me on GitHub</a>
+        <li class="github"><a href="http://github.com/KevBurnsJr/irc-log-viewer" target="_blank">Fork me on GitHub</a>
     </ul>
     
 <? include('footer.html.php'); ?>
