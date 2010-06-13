8c1e36ab27b492213ddfae1963df0279c8af6706
KevBurnsJr <kevburnsjr@gmail.com>
2010-06-13 03:13:10 -0700

    Fixing index.

--- views/index.html.php
+++ views/index.html.php
@@ -6,13 +6,13 @@
     <div class="dates">
     <?
         $maxsize = 0;
-        $files = array_reverse(scandir($logdir));
+        $files = array_reverse(array_slice(scandir($logdir),2));
         foreach($files as $file) {
-            $maxsize = max($maxsize,filesize($logdir."/".$file));
+            $maxsize = max($maxsize, filesize($logdir."/".$file));
         }
         foreach($files as $file) {
             if(strpos($file, $logprefix.'.log') > -1) {
-                $w = floor(filesize($logdir."/".$file)/$maxsize*100);
+                $w = number_format(filesize($logdir."/".$file)/$maxsize*100,1);
                 $filedate = substr($file, strlen($logprefix)+5);
                 ?> 
         <a href="<?=($baserel.$filedate)?>.html"><?=$filedate?></a>
