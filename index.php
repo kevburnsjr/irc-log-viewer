2efc69821737cbbe49dd1663327f96441a0e4b0c
kenjis <kenji.uui@gmail.com>
2014-01-31 14:40:18 +0900

    Add Charset and Lang settings

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 <?php
 
+ini_set('default_charset', 'UTF-8');
+mb_language('ja');
+mb_internal_encoding('UTF-8');
+//setlocale(LC_ALL, 'ja_JP.UTF-8');
+
 require_once('functions.php');
 
 // SetEnv APP_CHANNEL rest
